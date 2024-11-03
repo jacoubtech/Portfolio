f4d36f60dc9f628245c98916c648de0b801585ee
Fortas Nour El Islem Yaakoub <fortas.yacoub85@gmail.com>
2024-11-03 22:19:51 +0100

    Home page content and animation - done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button"
+
 import {FiDownload} from 'react-icons/fi'
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -9,7 +11,7 @@ const Home = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left order-1 xl: order-none">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">NextJS Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br/> <span className="text-accent">Fortas Islem</span>
@@ -30,11 +32,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div className="order-1 xl: order-none mb-8 xl:mb-0">
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   )
 }
