89bdd5681703d93466972655dacba4b203ce3250
Islem Fortas <fortas.yacoub85@gmail.com>
2024-11-05 15:34:51 +0100

    Services update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,46 @@
-const services = () => {
+"use client";
+
+import {BsArrowDownRight} from "react-icons/bs";
+import Link from "next/link";
+import { title } from "process";
+ 
+const services = [
+  {
+    num: '01',
+    title: 'Web Developement',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    href: ""
+  },
+  {
+    num: '02',
+    title: 'UI/UX Design',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    href: ""
+  },
+  {
+    num: 'Photo/Video-Graphy',
+    title: 'Web Developement',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    href: ""
+  },
+  {
+    num: '04',
+    title: 'Video Editing',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' ,
+    href: ""
+  },
+]
+
+
+import { motion
+
+ } from "framer-motion";
+const Services = () => {
   return (
-    <div>services page</div>
+    <section>
+      <div className="container mx-auto">services page</div>
+    </section>
   )
 }
 
-export default services
+export default Services
