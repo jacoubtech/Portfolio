f789cdb577104bfaa4a3ced6402304e9cb7a4103
Islem Fortas <fortas.yacoub85@gmail.com>
2024-11-06 15:52:10 +0100

    resume not yet done

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,61 @@
+"use client";
+
+import { Description } from "@radix-ui/react-dialog";
+import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from "react-icons/fa";
+
+import {SiTailwindcss, SiNextdotjs} from "react-icons/si";
+
+// about Data
+
+const about = {
+  title: 'About me',
+  Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam totam exercitationem unde esse numquam quas incidunt optio.",
+  info: [
+    {
+      fieldName: 'Name',
+      fieldValue: 'Islem Fortas',
+    },
+    {
+      fieldName: 'Phone',
+      fieldValue: '(+213) 6 99 99 80 15',
+    },
+    {
+      fieldName: 'Experience',
+      fieldValue: '+10 Years',
+    },
+    {
+      fieldName: 'Skype',
+      fieldValue: 'Islem Fortas',
+    },
+    {
+      fieldName: 'Nationality',
+      fieldValue: 'Algerian',
+    },
+    {
+      fieldName: 'Email',
+      fieldValue: 'contact@islem-fortas.site',
+    },
+    {
+      fieldName: 'Freelance',
+      fieldValue: 'Available',
+    },
+    {
+      fieldName: 'Languages',
+      fieldValue: 'Arabic , English , French',
+    },
+  ]
+};
+// about Experience
+
+const experience = {
+  title: 'About me',
+  Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam totam exercitationem unde esse numquam quas incidunt optio.",
+  info: []
+  };
 const resume = () => {
   return (
     <div>resume page</div>
   )
-}
+};
 
 export default resume;
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -42,20 +42,20 @@ const Services = () => {
                     }}
                     className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
                     >
-          {services.map((services, index)=> {
+          {services.map((service, index)=> {
             return (
               <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                 {/* top */}
               <div className="w-full flex justify-between items-center">
-                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{services.num}</div>
-                <Link href={services.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
                 <BsArrowDownRight className="text-primary text-3xl"/>
                 </Link>
               </div>
               {/* title */}
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{services.title}</h2>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
               {/* description */}
-              <p className="text-white/60">{services.description}</p>
+              <p className="text-white/60">{service.description}</p>
               {/* border */}
               <div className="border-b border-white/20 w-full"></div>
             </div>
