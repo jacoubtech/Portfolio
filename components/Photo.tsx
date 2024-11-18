318e55bc8969fe47913364666f47c59acabac298
Fortas Nour El Islem Yaakoub <fortas.yacoub85@gmail.com>
2024-11-18 06:45:48 +0100

    photo profile edit

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -15,7 +15,7 @@ const Photo = () => {
       }}
         className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten
         absolute">
-          <Image src="/assets/photo2.png" 
+          <Image src="/assets/islemj1.png" 
                  priority 
                  quality={100} 
                  fill 
