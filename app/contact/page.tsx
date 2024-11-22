97a1227266936ae82b0496203bbb1e034de37a1d
Fortas Nour El Islem Yaakoub <fortas.yacoub85@gmail.com>
2024-11-22 06:55:17 +0100

    contact page edit - remove fetching func from desk home

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,54 +26,7 @@ const info = [
 ];
 
 const contact = () => {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [service, setService] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
   
-    const form = {
-      firstname,
-      lastname,
-      email,
-      phone,
-      service,
-      message,
-    };
-  
-    try {
-      const response = await fetch('/api/submit', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(form),
-      });
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const content = await response.json();
-      console.log(content);
-    } catch (error) {
-      console.error("Fetch error: " );
-    }
-    // if (content.data && content.data.tableRange) {
-    //   alert(content.data.tableRange);
-    // } else {
-    //   alert('Submission successful, but no table range returned.');
-    // }
-  
-    setFirstname('');
-    setLastname('');
-    setEmail('');
-    setPhone('');
-    setService('');
-    setMessage('');
-  };
-
   return (
     <motion.section initial={{ opacity: 0 }} 
       animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
@@ -82,17 +35,17 @@ const contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={handleSubmit}>
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" >
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">Let's work together! I'm ready to leverage my extensive skills in web design, UI/UX, and multimedia production to help achieve your project goals with innovative and effective digital solutions.</p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input value={firstname} onChange={e => setFirstname(e.target.value)} type="text" placeholder="First Name" />
-                <Input value={lastname} onChange={e => setLastname(e.target.value)} type="text" placeholder="Last Name" />
-                <Input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Email Address" />
-                <Input value={phone} onChange={e => setPhone(e.target.value)} type="text" placeholder="Phone Number (WhatsApp)" />
+                <Input   type="text" placeholder="First Name" />
+                <Input   type="text" placeholder="Last Name" />
+                <Input   type="email" placeholder="Email Address" />
+                <Input   type="text" placeholder="Phone Number (WhatsApp)" />
               </div>
-              <Select onValueChange={value => setService(value)}>
+              <Select /*onValueChange={value => setService(value)}*/>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
@@ -106,7 +59,7 @@ const contact = () => {
                 </SelectContent>
               </Select>
               {/* text area */}
-              <Textarea value={message} className="h-[200px]" onChange={e => setMessage(e.target.value)} name="message" placeholder="Type Your message Here" />
+              <Textarea /*value={message}*/ className="h-[200px]" /*onChange={e => setMessage(e.target.value)}*/ name="message" placeholder="Type Your message Here" />
               <Button size="sm" className="max-w-40" type="submit">Send message</Button>
             </form>
           </div>
