ac0beebfe2ad4d419a8eafb49d5656e0fa23c760
Islem Fortas <fortas.yacoub85@gmail.com>
2024-12-18 12:55:28 +0100

    Done submit form

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -28,22 +28,36 @@ const info = [
 const contact = () => {
   
   return (
-    <motion.section initial={{ opacity: 0 }} 
-      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' } }}
-      className="py-6">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" >
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              action="https://formspree.io/f/mqakvneg"
+              method="POST"
+            >
               <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">Let's work together! I'm ready to leverage my extensive skills in web design, UI/UX, and multimedia production to help achieve your project goals with innovative and effective digital solutions.</p>
+              <p className="text-white/60">
+                Let's work together! I'm ready to leverage my extensive skills
+                in web design, UI/UX, and multimedia production to help achieve
+                your project goals with innovative and effective digital
+                solutions.
+              </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input   type="text" placeholder="First Name" />
-                <Input   type="text" placeholder="Last Name" />
-                <Input   type="email" placeholder="Email Address" />
-                <Input   type="text" placeholder="Phone Number (WhatsApp)" />
+                <Input type="text" placeholder="First Name" />
+                <Input type="text" placeholder="Last Name" />
+                <Input type="email" placeholder="Email Address" />
+                <Input type="text" placeholder="Phone Number (WhatsApp)" />
               </div>
               <Select /*onValueChange={value => setService(value)}*/>
                 <SelectTrigger className="w-full">
@@ -52,15 +66,25 @@ const contact = () => {
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Select a Service</SelectLabel>
-                    <SelectItem value="web-development">Web Development</SelectItem>
+                    <SelectItem value="web-development">
+                      Web Development
+                    </SelectItem>
                     <SelectItem value="ui-ux-design">UI/UX Design</SelectItem>
-                    <SelectItem value="social-media-design">Social Media Design</SelectItem>
+                    <SelectItem value="social-media-design">
+                      Social Media Design
+                    </SelectItem>
                   </SelectGroup>
                 </SelectContent>
               </Select>
               {/* text area */}
-              <Textarea /*value={message}*/ className="h-[200px]" /*onChange={e => setMessage(e.target.value)}*/ name="message" placeholder="Type Your message Here" />
-              <Button size="sm" className="max-w-40" type="submit">Send message</Button>
+              <Textarea
+                /*value={message}*/ className="h-[200px]"
+                /*onChange={e => setMessage(e.target.value)}*/ name="message"
+                placeholder="Type Your message Here"
+              />
+              <Button size="sm" className="max-w-40" type="submit">
+                Send message
+              </Button>
             </form>
           </div>
           {/* info */}
